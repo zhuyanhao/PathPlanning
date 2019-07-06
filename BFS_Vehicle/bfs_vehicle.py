aaccc4543c7e15608ea32bb4201f4b3dc0211c7d
yanhao <yhzhu@umich.edu>
2019-07-06 13:23:16 -0400

    Complete the code and add result files

--- BFS_Vehicle/bfs_vehicle.py
+++ BFS_Vehicle/bfs_vehicle.py
@@ -6,6 +6,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 from scipy.integrate import RK45
+from queue import Queue
+import csv
+
+class Node:
+    """
+    Node in search Graph. It stores the current configuration,
+    its parent and the operation u from parent to itself.
+    """
+    def __init__(self, x, y, theta, parent, u):
+        self.x = x
+        self.y = y
+        self.theta = theta
+        self.parent = parent
+        self.u = u   # left, right or straight
 
 class VehiclePlanner:
     
@@ -27,7 +41,8 @@ class VehiclePlanner:
         self.beta = np.arctan(0.5*np.tan(40/180*np.pi))
         self.dt = np.pi/2/(self.v*np.sin(self.beta)/self.lr)  # time step for integration. vehicle +- 90 degrees.
         self.r = 2.5  # The car is simplified as a sphere with radius = 2.5 in collision detection.
-        
+        self.oper = ["left", "right", "straight"]
+
         #Final Position
         self.xf = self.verCorridor / 2.0
         self.yf = 0.0
@@ -63,12 +78,32 @@ class VehiclePlanner:
         # Add the initial configuration to the plot
         plt.plot([self.width-self.verCorridor/2], [0], 'bo', markersize=10.0)
 
+        plt.title("Kinematic Bicycle Model - BFS")
         plt.xlim(-10, 110)
         plt.ylim(-10, 110)
         plt.draw()
         plt.pause(1e-3)
 
         return ax
+
+    def plotTraj(self, x, y):
+        """
+        Plot the trajecory and add a big circle at the end.
+        This should be called when a new valid configuration is found.
+        """
+        self.ax.plot(x, y, 'b:', linewidth=4)
+        self.ax.plot([x[-1]], [y[-1]], 'bo', markersize=10.0)
+        plt.draw()
+        plt.pause(1e-3)
+    
+    def plotCross(self, x, y):
+        """
+        Plot a 'x' at the beginning of the trajectory.
+        This should be called when the new trajectory is invalid.
+        """
+        self.ax.plot([x], [y], 'rx', markersize=10.0)
+        plt.draw()
+        plt.pause(1e-3)
 
     def turnLeft(self, t, y):
         """
@@ -126,14 +161,14 @@ class VehiclePlanner:
 
         integrator = RK45(fun=dx_dt, t0=0, y0=states, t_bound=self.dt, max_step=self.dt/20)
         result = [states]
-        print ("="*50)
-        print ("Operation = ", oper)
-        print ("t = 0.0, states=", states)
+        # print ("="*50)
+        # print ("Operation = ", oper)
+        # print ("t = 0.0, states=", states)
         while integrator.status == "running":
             integrator.step()
             states = [integrator.y[0], integrator.y[1], integrator.y[2]]
             result.append(states)
-            print ("t = {}, states=".format(integrator.t), states)
+            # print ("t = {}, states=".format(integrator.t), states)
             if self.collide(states[0], states[1]):
                 integrator.status = "failed"
 
@@ -146,6 +181,10 @@ class VehiclePlanner:
         """
         Return true if collision is detected at the given point.
         """
+        # It should never goes above the upper bound
+        if x >= self.height:
+            return True
+
         # Check the left outer boundary
         if x <= 0 + self.r:
             return True
@@ -164,11 +203,98 @@ class VehiclePlanner:
         
         return False
 
-    
+    def plan(self):
+        """
+        Run BFS to find a feasible path
+        """
+        q = Queue()
+        init = Node(self.x0, self.y0, self.theta0, None, "")
+        q.put(init)
+        self.nodes = [init]
+
+        while not q.empty():
+            # Take all points in one single interation
+            size = q.qsize()
+            for _ in range(size):
+                node = q.get()
+                x = node.x
+                y = node.y
+                theta = node.theta
+                find_next = False
+                for oper in self.oper:
+                    try:
+                        result = self.simulate(x, y, theta, oper)
+                        new_node = Node(
+                            x = result[-1][0],
+                            y = result[-1][1],
+                            theta = result[-1][2],
+                            parent = node,
+                            u = oper
+                            )
+                        if self.visited(new_node):
+                            raise RuntimeError("The node has already been visited.")
+                        find_next = True
+                        traj_x = []
+                        traj_y = []
+                        for step in result:
+                            traj_x.append(step[0])
+                            traj_y.append(step[1])
+                        self.plotTraj(traj_x, traj_y)
+                        q.put(new_node)
+                        self.nodes.append(new_node)
+                        print ("Add new node: x = {}, y = {}".format(traj_x[-1], traj_y[-1]))
+                        if self.findGoal(new_node.x, new_node.y):
+                            plt.savefig('vehicle.png')
+                            return new_node
+                    except:
+                        pass
+                
+                if not find_next:
+                    self.plotCross(x, y)
+
+        raise RuntimeError("Cannot find a feasible path to the goal.")
+
+    def findGoal(self, x, y):
+        if y <= 0 and 0 <= x <= self.verCorridor:
+            return True
+        else:
+            return False
+
+    def visited(self, new_node):
+        """
+        Check if the new node is close to any visited node
+        """
+        for node in self.nodes:
+            dist = np.sqrt((node.x-new_node.x)**2 + (node.y-new_node.y)**2)
+            if dist < 5:
+                return True
+        
+        return False
 
 if __name__ == "__main__":
     v_planner = VehiclePlanner()
-    print ("Initialization complete")
-    v_planner.simulate(90, 0, np.pi/2, "left")
-    v_planner.simulate(90, 0, np.pi/2, "right")
-    v_planner.simulate(95, 0, np.pi/2, "straight")
+    node = v_planner.plan()
+    
+    # Restore the history
+    x = []
+    y = []
+    theta = []
+    oper = []
+    t = [0]
+    while node is not None:
+        x.insert(0, node.x)
+        y.insert(0, node.y)
+        theta.insert(0, node.theta)
+        oper.insert(0, node.u)
+        t.append(t[-1] + v_planner.dt)
+        node = node.parent
+
+    # Operation needs to move one element forward
+    del oper[0]
+    oper.append("")
+    with open("vehicle_oper.csv", "w") as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        # Write the header
+        writer.writerow(["Time", "X", "Y", "Theta", "Operation"])
+        for i in range(len(x)):
+            writer.writerow([t[i], x[i], y[i], theta[i], oper[i]])
